e4ad5d13f9a60f8a5db3305c93522da64d2fc2f1
Abe <abe_vos@msn.com>
2018-09-27 11:26:52 +0200

    Create build script in Python

---
+++ assignmentfiles_2017/build.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import os
+
+from subprocess import call
+
+class_blacklist = ['BentCigarFunction.class',
+                   'KatsuuraEvaluation.class',
+                   'SchaffersEvaluation.class',
+                   'SphereEvaluation.class']
+
+paths = os.listdir('./')
+
+java_files = []
+class_files = []
+
+for path in paths:
+    filename, extension = os.path.splitext(path)
+
+    if extension == '.java':
+        java_files.append(path)
+
+print(f"Compiling {', '.join(java_files)}.")
+call(["javac", "-cp", "contest.jar"] + java_files)
+
+for path in paths:
+    filename, extension = os.path.splitext(path)
+
+    if extension == '.class' and path not in class_blacklist:
+        class_files.append(path)
+
+call(["jar", "cmf", "MainClass.txt", "submission.jar"] + class_files)
